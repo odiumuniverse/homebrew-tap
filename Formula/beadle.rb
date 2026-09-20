2b4b496e362e20f74d22b2a912f84576ade3805d
Azamat <azamatigruha@gmail.com>
2026-09-21 02:17:25 +0400

    beadle: install the prebuilt tarball and manage the watcher service

--- Formula/beadle.rb
+++ Formula/beadle.rb
@@ -1,14 +1,28 @@
 class Beadle < Formula
   desc "Sync AI coding agent config from one private vault"
   homepage "https://github.com/odiumuniverse/beadle"
-  url "https://github.com/odiumuniverse/beadle.git", tag: "v0.1.0"
+  url "https://github.com/odiumuniverse/beadle/releases/download/v0.2.0/beadle-0.2.0-darwin-universal.tar.gz"
+  sha256 "871353e3902ab8fd9fcd51959cd9c9db4ee4d03465cd24be5d92aa828190bb9e"
   license "MIT"
-  head "https://github.com/odiumuniverse/beadle.git", branch: "master"
-
-  depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}", output: bin/"beadle"), "./cmd/beadle"
+    bin.install "beadle"
+  end
+
+  service do
+    run [opt_bin/"beadle", "watch"]
+    keep_alive true
+    log_path var/"log/beadle.log"
+    error_log_path var/"log/beadle.err.log"
+    working_dir var
+  end
+
+  def caveats
+    <<~EOS
+      Run `brew services start beadle` to keep the watcher running.
+      It serves the default vault (~/.beadle); for a custom vault use
+      `beadle daemon install` instead.
+    EOS
   end
 
   test do
